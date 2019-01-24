c096f66beb25a2436410d5f9a6364d8e5d551fc2
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-24 08:45:29 -0800

    added validatename and create to items.js

--- scripts/items.js
+++ scripts/items.js
@@ -1,5 +1,22 @@
 'use strict'; 
+
 const Item = (function() {
-  const foo = 'bar'; 
-  return {}; 
+  
+	function validateName(name){
+		if (!name){
+			throw new TypeError('Name does not exist.');
+		}
+	}  
+	function create(name){
+		return {
+			id: cuid(),
+			name,
+			checked: false
+		};
+	}
+
+	return {
+		validateName,
+		create
+	}; 
 }());
