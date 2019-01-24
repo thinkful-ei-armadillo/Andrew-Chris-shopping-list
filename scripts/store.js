f4237337899b842bfef1b3ce3755f1d16d154f8e
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-24 11:57:48 -0800

    added CRUD to store module

--- scripts/store.js
+++ scripts/store.js
@@ -27,11 +27,35 @@ const store = (function () {
       throw new Error(e.message); 
     }
   }
+  
+  function findAndToggleChecked(id){
+    this.findByID(id).checked = !this.findByID(id).checked;
+  }
+
+  function findAndUpdateName(id, newName){
+    try{
+      Item.validateName(newName);
+      this.findByID(id).name = newName;
+    }
+    catch(error){
+      throw new Error(`Cannot update name: ${error.message}`);
+    }
+  }
+
+  function findAndDelete(id){
+    this.items = this.items.filter(function(item){
+      return item.id !== id;
+    });
+  }
+
   return {
     items,
     hideCheckedItems,
     searchTerm,
     findByID, 
-    addItem, 
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   }; 
 }() );
